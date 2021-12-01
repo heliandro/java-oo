bd017cb482e5a8ec854c022321987b4515f40d26
Heliandro <heliandro.l@gmail.com>
2021-12-01 17:51:04 -0300

    test: test the Funcionario class

--- src/App.java
+++ src/App.java
@@ -1,20 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Cliente heliandro = new Cliente("Heliandro", "555.444.222.77", "Tech Lead");
-        Cliente kivia = new Cliente("Kivia", "111.333.888.55", "Designer");
-
-        Conta contaDoHeliandro = new Conta(1234, 77721, heliandro);
-        Conta contaDaKivia = new Conta(1234, 45522, kivia);
-
-        contaDoHeliandro.deposita(700);
-        contaDaKivia.deposita(500);
+        Funcionario heliandro = new Funcionario();
         heliandro.setNome("Heliandro Lavor");
+        heliandro.setCpf("223355646-9");
+        heliandro.setSalario(5000.80);
 
-        System.out.println(contaDoHeliandro.getNumero());
-        System.out.println(contaDoHeliandro.getTitular().getNome());
-        contaDoHeliandro.getTitular().setProfissao("Dev Master");
-        System.out.println(contaDoHeliandro.getTitular().getProfissao());
-        System.out.println(Conta.getTotal());
+        System.out.println(heliandro.getNome());
+        System.out.println(heliandro.getBonificacao());
     }
 }

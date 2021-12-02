d7c4b99aa259caab49b44ac695e45fb051b488f2
Heliandro <heliandro.l@gmail.com>
2021-12-02 15:56:15 -0300

    refactor: introduces new inheritance between Gerente and Administrator for auth

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,22 +1,8 @@
-public class Gerente extends Funcionario {
-    
-    private int senha;
-
-
-    public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        }
-        return false;
-    }
+public class Gerente extends FuncionarioAutenticavel {
 
     public double getBonificacao() {
         System.out.println("chamou o metodo getBonificacao do gerente");
         return super.getSalario();
-    }
-
-    public void setSenha(int senha) {
-        this.senha = senha;
     }
 
 }

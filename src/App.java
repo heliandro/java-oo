caac6eaba798baef4fa87c705fdade6a16ae9706
Heliandro <heliandro.l@gmail.com>
2021-12-02 14:15:41 -0300

    refactor: clean App class

--- src/App.java
+++ src/App.java
@@ -1,24 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Gerente heliandro = new Gerente();
-        heliandro.setNome("Heliandro");
-        heliandro.setSalario(12000.0);
-
-        Funcionario boechat = new Funcionario();
-        boechat.setNome("Boechat");
-        boechat.setSalario(15000.0);
-
-        LiderTecnico henrique = new LiderTecnico();
-        henrique.setNome("Henrique");
-        henrique.setSalario(13000.0);
-
-        ControleBonificacao controle = new ControleBonificacao();
-
-        controle.registra(heliandro);
-        controle.registra(boechat);
-        controle.registra(henrique);
-
-        System.out.println(controle.getSoma());
     }
 }

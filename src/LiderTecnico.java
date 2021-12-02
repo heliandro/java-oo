ad083a7a5ea70c0fec7cc445f33f2d8ecbcac0db
Heliandro <heliandro.l@gmail.com>
2021-12-02 15:05:02 -0300

    refactor: add abstract methods in childs of Funcionario class

--- src/LiderTecnico.java
+++ src/LiderTecnico.java
@@ -2,7 +2,7 @@ public class LiderTecnico extends Funcionario {
     
     public double getBonificacao() {
         System.out.println("chamou o metodo getBonificacao do lider tecnico");
-        return super.getBonificacao() + 100;
+        return 100;
     }
 
 }

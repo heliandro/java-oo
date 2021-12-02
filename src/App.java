fa9b88f24a4fcf5ef18944a1bd1ab8271a50642f
Heliandro <heliandro.l@gmail.com>
2021-12-01 22:46:23 -0300

    test: test the Gerente class

--- src/App.java
+++ src/App.java
@@ -3,10 +3,24 @@ public class App {
         
         Funcionario heliandro = new Funcionario();
         heliandro.setNome("Heliandro Lavor");
-        heliandro.setCpf("223355646-9");
+        heliandro.setCpf("2233556469");
         heliandro.setSalario(5000.80);
 
         System.out.println(heliandro.getNome());
         System.out.println(heliandro.getBonificacao());
+
+        Gerente ramon = new Gerente();
+        ramon.setNome("Ramonzito");
+        ramon.setCpf("55577744499");
+        ramon.setSalario(9000);
+
+        System.out.println(ramon.getNome());
+        System.out.println(ramon.getBonificacao());
+
+        ramon.setSenha(2222);
+
+        boolean autenticou = ramon.autentica(2222);
+
+        System.out.println(autenticou);
     }
 }

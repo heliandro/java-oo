8d98a687153838fee82de7b36d3f30e15d216037
Heliandro <heliandro.l@gmail.com>
2021-12-13 20:28:43 -0300

    feat: add connection feature

---
+++ src/TesteConexao.java
@@ -0,0 +1,28 @@
+public class TesteConexao {
+    public static void main(String[] args) {
+        
+        // esse novo recurso já usa um finally implicito
+        try ( Conexao conexao = new Conexao() ) {
+            conexao.leDados();
+        } catch(IllegalStateException ex) {
+            System.out.println("Deu erro na conexao");
+        }
+
+        // ---------------------
+
+        // Conexao con = null;
+        // try {
+        //     con = new Conexao();
+        //     con.leDados();
+            
+        // } catch(IllegalStateException ex) {
+        //     System.out.println("Deu erro na conexao");
+        //     ex.printStackTrace();
+        // } finally {
+        //     System.out.println("finaly");
+        //     if (con != null) {
+        //         con.fecha();
+        //     }
+        // }
+    }
+}

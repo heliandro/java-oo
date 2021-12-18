db8702df3cc2653c306cb41a44e20b209a12e8c0
Heliandro <heliandro.l@gmail.com>
2021-12-18 14:29:05 -0300

    refactor: update account feature

--- src/br/com/bytebank/banco/modelo/Conta.java
+++ src/br/com/bytebank/banco/modelo/Conta.java
@@ -82,4 +82,9 @@ public abstract class Conta {
     public static int getTotal() {
         return Conta.total;
     }
+
+    @Override
+    public String toString() {
+        return "Agencia: " + this.agencia + " | Numero: " + this.numero;
+    }
 }

0214dda98eda26399e038f35d1a32b57a22183ca
Heliandro <heliandro.l@gmail.com>
2021-12-21 22:39:39 -0300

    test: java.util order by comparable element

--- src/br/com/bytebank/banco/modelo/Conta.java
+++ src/br/com/bytebank/banco/modelo/Conta.java
@@ -6,7 +6,7 @@ package br.com.bytebank.banco.modelo;
  * @author Heliandro
  * 
  */
-public abstract class Conta {
+public abstract class Conta implements Comparable<Conta> {
 
     protected double saldo;
     private int agencia;
@@ -93,6 +93,11 @@ public abstract class Conta {
         }
 
         return true;
+    }
+
+    @Override
+    public int compareTo(Conta outra) {
+        return Double.compare(this.saldo, outra.getSaldo());
     }
 
     @Override

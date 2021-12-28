16e4ff0ae45e815a71ffb0a50a55bb14838de4d9
Heliandro <heliandro.l@gmail.com>
2021-12-27 23:04:25 -0300

    test: copy content from file to another

---
+++ src/br/com/bytebank/banco/teste/io/TesteCopiarArquivo.java
@@ -0,0 +1,36 @@
+package br.com.bytebank.banco.teste.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class TesteCopiarArquivo {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        Reader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        String linha = br.readLine();
+
+        while(linha != null) {
+            bw.write(linha);
+            bw.newLine();
+            linha = br.readLine();
+        }
+
+        bw.close();
+        br.close();
+    }
+}

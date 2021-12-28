ba3db7d1655748b76babf6c6d2e5948bc1554785
Heliandro <heliandro.l@gmail.com>
2021-12-27 23:01:53 -0300

    test: update TesteLeitura

--- src/br/com/bytebank/banco/teste/io/TesteLeitura.java
+++ src/br/com/bytebank/banco/teste/io/TesteLeitura.java
@@ -2,18 +2,16 @@ package br.com.bytebank.banco.teste.io;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TesteLeitura {
     public static void main(String[] args) throws IOException {
-        
 
         // fluxo de entrada com arquivo
         FileInputStream fis = new FileInputStream("lorem.txt");
-        InputStreamReader isr = new InputStreamReader(fis); // transforma bytes em characteres
-        BufferedReader br = new BufferedReader(isr); // transformando characters para buffer de caracteres
+        InputStreamReader isr = new InputStreamReader(fis); // transforma array de bytes em characteres
+        BufferedReader br = new BufferedReader(isr); // transformando array de characters para buffer de caracteres
 
         String linha = br.readLine();
 
